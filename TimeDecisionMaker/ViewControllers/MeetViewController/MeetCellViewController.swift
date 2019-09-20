43a2021b4289fe480cc5124d24817ac17e20d0ec
Константин Овчаренко <wowbufi@gmail.com>
2019-09-20 21:48:25 +0300

    New flow, remove all old ui

---
+++ TimeDecisionMaker/ViewControllers/MeetViewController/MeetCellViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MeetCellViewController.swift
+//  TimeDecisionMaker
+//
+//  Created by Константин Овчаренко on 9/20/19.
+//
+
+import UIKit
+
+class MeetCellViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
