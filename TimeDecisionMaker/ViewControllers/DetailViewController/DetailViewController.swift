eee6865d62e179e1640808bb281cf485806fc6ce
Константин Овчаренко <wowbufi@gmail.com>
2019-10-12 03:21:15 +0300

    Done DetailVC on 80%

--- TimeDecisionMaker/ViewControllers/DetailViewController/DetailViewController.swift
+++ TimeDecisionMaker/ViewControllers/DetailViewController/DetailViewController.swift
@@ -8,22 +8,81 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    let event = Meet()
+    let user = User()
+    let service = RDFileServise()
+    
+    let timeZone = "Europe/Kiev"
+    let format = "dd/MM/yyyy HH:mm"
+    
 
+    @IBOutlet weak var TitleLabel: UILabel!
+    
+    @IBOutlet weak var startMeetinTextField: UITextField!
+    
+    @IBOutlet weak var endMeetingTextField: UITextField!
+    
+    @IBOutlet weak var LastChangeDate: UILabel!
+    
+    @IBOutlet weak var CreateMeetingDate: UILabel!
+    
+    @IBOutlet weak var StatusLabel: UILabel!
+    
+    @IBOutlet weak var securetyLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        viewVC()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillDisappear(_ animated: Bool) {
+        updateMeet()
     }
-    */
-
+    
+    func viewVC() {
+        
+        self.TitleLabel.text = event.summary
+        self.startMeetinTextField.text = event.dateInterval.start.hoursValueFromDateToString()
+        self.endMeetingTextField.text = event.dateInterval.end.hoursValueFromDateToString()
+        self.LastChangeDate.text = event.lastModified.hoursValueFromDateToString()
+        self.CreateMeetingDate.text = event.created.hoursValueFromDateToString()
+ //       self.StatusLabel.text = event.status
+        
+        if self.securetyLabel.isHidden == true {
+             let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress))
+             lpgr.minimumPressDuration = 0.5
+             lpgr.delaysTouchesBegan = true
+             lpgr.delegate = self as? UIGestureRecognizerDelegate
+             self.securetyLabel.addGestureRecognizer(lpgr)
+        }
+        
+    }
+    
+    func updateMeet(){
+        
+        if service.chekTime(startTime: (startMeetinTextField.text?.convertStringToDate(timezone: timeZone, format: format))!, endTime: (endMeetingTextField.text?.convertStringToDate(timezone: timeZone, format: format))!) {
+            
+            if service.saveEventChanges(event: event, resourceFile: user.ICSFile, resourseFile2: nil) {
+                print("Your meet were resaved")
+            } else{
+                print("Somesing gone wrong")
+            }
+        }
+        
+        
+    }
+    
+    //MARK: - UILongPressGestureRecognizer Action -
+    
+    @objc func handleLongPress(gestureReconizer: UILongPressGestureRecognizer) {
+        if gestureReconizer.state != UIGestureRecognizer.State.ended {
+            //When lognpress is start or running
+        }
+        else {
+            self.view.removeFromSuperview()
+        }
+    }
+    
 }
